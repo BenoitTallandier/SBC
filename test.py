abd230df67f2bf62aca06b33c177c08375ab48a8
MorelDoridat Stefan <stefan.morel-doridat@telecomnancy.eu>
2018-03-12 17:29:57 +0100

    fin projet

--- test.py
+++ test.py
@@ -28,6 +28,7 @@ def createInstance (nameInstance,className):
     f.edge(nameInstance,className,'rdf:type')
 
 def createRelation (relationRange,domain,relationName):
+    f.attr('node', shape='circle')
     f.node(relationRange)
     f.node(domain)
     f.edge(relationRange,domain,relationName)
@@ -37,84 +38,85 @@ def createRelation (relationRange,domain,relationName):
 g=rdflib.Graph()
 g.load('ontology-rdf.owl')
 
-classMoreInstancied = g.query(
-    """
-    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-    PREFIX owl: <http://www.w3.org/2002/07/owl#>
-    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-    SELECT ?class (COUNT(?sub) as ?subCount)
-    WHERE {
-        ?sub rdf:type ?type.
-        ?sub a ?class.
-        FILTER( ?class = owl:NamedIndividual).
-        FILTER (?type != owl:NamedIndividual)
-    }
-    GROUP BY ?class
-    ORDER BY DESC(?subCount)
-    """+("LIMIT "+str(limitInstance) if(int(limitInstance))>0 else "")
-)
-
-individues = g.query(
-    """
-    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-    PREFIX owl: <http://www.w3.org/2002/07/owl#>
-    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-    SELECT ?sub ?type (COUNT(?sub) as ?subCount)
-    WHERE {
-        ?sub rdf:type ?type.
-        ?sub a ?class.
-        FILTER( ?class = owl:NamedIndividual).
-        FILTER (?type != owl:NamedIndividual)
-    }
-    GROUP BY ?sub
-    ORDER BY DESC(?subCount)
-    """+("LIMIT "+str(limitInstance) if(int(limitInstance))>0 else ""))
-
 instances = []
 classes = []
 classesNames = []
 properties = []
 propertiesEffective = []
-
 me = False
 
-for row in individues:
-    if(len(row[0].split("#"))>1 and len(row[1].split("#"))>1):
-        instance = row[0].split("#")[1]
-        className = row[1].split("#")[1]
+classMoreInstancied = """
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX owl: <http://www.w3.org/2002/07/owl#>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+    SELECT ?type (COUNT(?sub) as ?subCount)
+    WHERE {
+        ?sub rdf:type ?type.
+        ?sub a ?class.
+        FILTER( ?class = owl:NamedIndividual).
+        FILTER (?type != owl:NamedIndividual)
+    }
+    GROUP BY ?type
+    ORDER BY DESC(?subCount)
+    """+("LIMIT "+str(limitInstance) if(int(limitInstance))>0 else "")
+#print (classMoreInstancied)
+classMoreInstancied = g.query(classMoreInstancied)
 
-        instances.append((instance,className))
-        if me==False:
-            me = row[1].split("#")[0]
+for cla in classMoreInstancied:
+    if me==False:
+        me = cla[0].split("#")[0]
+    individues = """
+            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            PREFIX owl: <http://www.w3.org/2002/07/owl#>
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+            PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+            PREFIX me: <"""+me+"""#>
+            SELECT ?sub ?type
+            WHERE {
+                ?sub rdf:type ?type.
+                ?sub a me:"""+cla[0].split("#")[1]+""".
+                FILTER (?type != owl:NamedIndividual)
+                }"""
+    #print (individues)
+    individues = g.query(individues)
 
-        test = """
-        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        PREFIX owl: <http://www.w3.org/2002/07/owl#>
-        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-        PREFIX me: <"""+me+"""#>
-        SELECT ?class
-        WHERE {
-            me:"""+className+""" rdfs:subClassOf ?class.
-        }"""
 
-        #print test
+    for row in individues:
+        if(len(row[0].split("#"))>1 and len(row[1].split("#"))>1):
+            instance = row[0].split("#")[1]
+            className = row[1].split("#")[1]
 
-        newClassesMere = g.query(test)
+            instances.append((instance,className))
+            if me==False:
+                me = row[1].split("#")[0]
 
-        if className not in classesNames:
-            classesNames.append(className)
+            test = """
+            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            PREFIX owl: <http://www.w3.org/2002/07/owl#>
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+            PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+            PREFIX me: <"""+me+"""#>
+            SELECT ?class
+            WHERE {
+                me:"""+className+""" rdfs:subClassOf ?class.
+            }"""
 
-        for motherClass in newClassesMere:
-            if(len(motherClass[0].split("#"))>1):
-                motherClassName = motherClass[0].split("#")[1]
-                if((className,motherClassName) not in classes):
-                    classes.append((className,motherClassName))
+            #print test
 
-                if motherClassName not in classesNames:
-                    classesNames.append(motherClassName)
+            newClassesMere = g.query(test)
+
+            if className not in classesNames:
+                classesNames.append(className)
+
+            for motherClass in newClassesMere:
+                if(len(motherClass[0].split("#"))>1):
+                    motherClassName = motherClass[0].split("#")[1]
+                    if((className,motherClassName) not in classes):
+                        classes.append((className,motherClassName))
+
+                    if motherClassName not in classesNames:
+                        classesNames.append(motherClassName)
 
 
 for classe in classes:
@@ -259,20 +261,42 @@ for prop in properties:
 
 if(afficherABox):
     for classe in instances:
-        createInstance(classe[0],classe[1])
+        for c in classes:
+            if(c[0]==classe[1] or classe[1] == c[1]):
+                createInstance(classe[0],classe[1])
+                break
         #print "%s instance of %s" %(classe[0],classe[1])
 
 for prop in properties:
-    #print "prop:%s range:%s dom:%s" %(prop[0],prop[1],prop[2])
-    createRelation(prop[1],prop[2],prop[0])
+    print "prop:%s range:%s dom:%s" %(prop[0],prop[1],prop[2])
+    prop1OK = False;
+    prop2OK = False;
+    for c in classes:
+        if (prop[1] == c[0] or prop[1] == c[1]):
+            prop1OK = True
+        if (prop[2] == c[0] or prop[2] == c[1]):
+            prop2OK = True
+        if(prop1OK and prop2OK):
+            createRelation(prop[1],prop[2],prop[0])
+            break
 
 if(afficherABox):
     for prop in propertiesEffective:
         #print "d:%s p:%s r:%s" %(prop[0],prop[1],prop[2])
-        createRelation(prop[0],prop[2],prop[1])
+        prop0OK = False
+        prop2OKK =False
+        for i in instances :
+            if (prop[0] == i[0]):
+                prop0OK = True
+            if (prop[2] == i[0]):
+                prop2OKK = True
+            if (prop0OK and prop2OKK):
+                createRelation(prop[0],prop[2],prop[1])
+                break
+        #if (prop[0] in instances and prop[2] in instances):
 
 for classe in classes:
-    #print "%s subClassOf of %s" %(classe[0],classe[1])
+    print "%s subClassOf of %s" %(classe[0],classe[1])
     createClass(classe[0],classe[1])
 
 for classe in classesNames:
