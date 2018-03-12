120bbf866ebf1bc5bbe55390f1462a79d42adfbf
Florian FREI <florian.frei@telecomnancy.eu>
2018-03-12 11:00:50 +0100

    prop

--- test.py
+++ test.py
@@ -46,7 +46,10 @@ individues = g.query(
 
 instances = []
 classes = []
-classesMere = []
+classesNames = []
+properties = []
+
+me = False
 
 for row in individues:
     if(len(row[0].split("#"))>1 and len(row[1].split("#"))>1):
@@ -54,7 +57,8 @@ for row in individues:
         className = row[1].split("#")[1]
 
         instances.append((instance,className))
-        me = row[1].split("#")[0]
+        if me==False:
+            me = row[1].split("#")[0]
 
         test = """
         PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
@@ -69,23 +73,85 @@ for row in individues:
 
         newClassesMere = g.query(test)
 
+        if className not in classesNames:
+            classesNames.append(className)
+
         for motherClass in newClassesMere:
             if(len(motherClass[0].split("#"))>1):
                 motherClassName = motherClass[0].split("#")[1]
                 if((className,motherClassName) not in classes):
                     classes.append((className,motherClassName))
 
-        if len(newClassesMere)==0 :
-            if className not in classesMere:
-                classesMere.append(className)
+                if motherClassName not in classesNames:
+                    classesNames.append(motherClassName)
 
 for classe in instances:
-    print "%s instance of %s" %(classe[0],classe[1])
+    #print "%s instance of %s" %(classe[0],classe[1])
+    pass
 
 for classe in classes:
-    print "%s subClassOf of %s" %(classe[0],classe[1])
+    className = classe[1]
+    test = """
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX owl: <http://www.w3.org/2002/07/owl#>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+    PREFIX me: <"""+me+"""#>
+    SELECT ?class
+    WHERE {
+        me:"""+className+""" rdfs:subClassOf ?class.
+    }"""
 
-for classe in classesMere:
+    newClassesMere = g.query(test)
+
+    for motherClass in newClassesMere:
+        if(len(motherClass[0].split("#"))>1):
+            motherClassName = motherClass[0].split("#")[1]
+            if((className,motherClassName) not in classes):
+                classes.append((className,motherClassName))
+
+            if motherClassName not in classesNames:
+                classesNames.append(className)
+
+for className in classesNames:
+
+    propertiesQuery = """
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX owl: <http://www.w3.org/2002/07/owl#>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+    PREFIX me: <"""+me+"""#>
+    SELECT DISTINCT ?p ?r ?d
+    WHERE {
+    {?p rdfs:range me:"""+className+"""} UNION {?p rdfs:domain me:"""+className+"""}.
+    ?p rdfs:range ?r.
+    ?p rdfs:domain ?d.
+    }"""
+
+    rows = g.query(propertiesQuery)
+
+    for row in rows:
+        if(len(row[0].split("#"))>1):
+            propertyName = row[0].split("#")[1]
+        if(len(row[0].split("#"))>1):
+            rangeName = row[1].split("#")[1]
+        if(len(row[0].split("#"))>1):
+            domainName = row[2].split("#")[1]
+        newRow = (propertyName, rangeName, domainName)
+
+        if newRow not in properties:
+            properties.append(newRow)
+
+
+
+for prop in properties:
+    print "prop:%s range:%s dom:%s" %(prop[0],prop[1],prop[2])
+
+for classe in classes:
+    #print "%s subClassOf of %s" %(classe[0],classe[1])
+    pass
+
+for classe in classesNames:
     print "%s " %(classe)
 
 
