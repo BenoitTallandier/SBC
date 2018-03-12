2411a79453e4dc3c3a43262b90d41dadc279c6bb
Florian FREI <florian.frei@telecomnancy.eu>
2018-03-12 10:25:08 +0100

    maman

--- test.py
+++ test.py
@@ -30,13 +30,12 @@ f.edge('LR_8', 'LR_5', label='S(a)')
 g=rdflib.Graph()
 g.load('ontology-rdf.owl')
 
-qres = g.query(
+individues = g.query(
     """
     PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
     PREFIX owl: <http://www.w3.org/2002/07/owl#>
     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
     PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-    PREFIX me: <http://www.semanticweb.org/teset1u/ontologies/2018/2/untitled-ontology-11#>
     SELECT ?sub ?type
     WHERE {
         ?sub rdf:type ?type.
@@ -44,11 +43,51 @@ qres = g.query(
       FILTER( ?class = owl:NamedIndividual).
       FILTER (?type != owl:NamedIndividual)
     }""")
-for row in qres:
+
+instances = []
+classes = []
+classesMere = []
+
+for row in individues:
     if(len(row[0].split("#"))>1 and len(row[1].split("#"))>1):
         instance = row[0].split("#")[1]
         className = row[1].split("#")[1]
-        print("%s is %s" %(instance, className))
-print(len(qres))
+
+        instances.append((instance,className))
+        me = row[1].split("#")[0]
+
+        test = """
+        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX owl: <http://www.w3.org/2002/07/owl#>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+        PREFIX me: <"""+me+"""#>
+        SELECT ?class
+        WHERE {
+            me:"""+className+""" rdfs:subClassOf ?class.
+        }"""
+
+        newClassesMere = g.query(test)
+
+        for motherClass in newClassesMere:
+            if(len(motherClass[0].split("#"))>1):
+                motherClassName = motherClass[0].split("#")[1]
+                if((className,motherClassName) not in classes):
+                    classes.append((className,motherClassName))
+
+        if len(newClassesMere)==0 :
+            if className not in classesMere:
+                classesMere.append(className)
+
+for classe in instances:
+    print "%s instance of %s" %(classe[0],classe[1])
+
+for classe in classes:
+    print "%s subClassOf of %s" %(classe[0],classe[1])
+
+for classe in classesMere:
+    print "%s " %(classe)
+
+
 # for s,p,o in g:
 #    print s,p,o
